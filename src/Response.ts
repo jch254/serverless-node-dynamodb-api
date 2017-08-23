9df8150b3ac1e6208714af008931203695575115
Jordan Hornblow <jordan@jch254.com>
2017-08-24 00:10:26 +1000

    Fix response headers

--- src/Response.ts
+++ src/Response.ts
@@ -11,11 +11,14 @@ export default class Response {
   statusCode: number;
   body?: string;
   headers: {
-    'Access-Control-Allow-Origin': '*', // Required for CORS
+    [name: string] : string,
   };
 
   constructor(args: ResponseArgs = defaultResponseArgs) {
     this.statusCode = args.statusCode;
+    this.headers = {
+      'Access-Control-Allow-Origin': '*', // Required for CORS
+    };
 
     if (args.body !== undefined) {
       this.body = JSON.stringify(args.body);
