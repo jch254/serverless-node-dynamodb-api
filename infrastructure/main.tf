ddabb7a934eca838750521717452aadaf78c2f47
Jordan Hornblow <jordan@jch254.com>
2019-09-20 18:13:26 +1000

    Update codebuild-project module to enable build badge

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,48 +33,48 @@ data "template_file" "codebuild_policy" {
   template = file("./codebuild-role-policy.tpl")
 
   vars = {
-    kms_key_arns = var.kms_key_arns
+    kms_key_arns       = var.kms_key_arns
     ssm_parameter_arns = var.ssm_parameter_arns
   }
 }
 
 resource "aws_iam_role_policy" "codebuild_policy" {
-  name = "${var.name}-codebuild-policy"
-  role = aws_iam_role.codebuild_role.id
+  name   = "${var.name}-codebuild-policy"
+  role   = aws_iam_role.codebuild_role.id
   policy = data.template_file.codebuild_policy.rendered
 }
 
 resource "aws_codebuild_webhook" "codebuild_webhook" {
-  project_name = var.name
+  project_name  = var.name
   branch_filter = "master"
 }
 
 module "codebuild_project" {
-  source = "github.com/jch254/terraform-modules//codebuild-project?ref=1.0.5"
+  source = "github.com/jch254/terraform-modules//codebuild-project?ref=1.0.6"
 
-  name = var.name
+  name               = var.name
   codebuild_role_arn = aws_iam_role.codebuild_role.arn
   build_docker_image = var.build_docker_image
-  build_docker_tag = var.build_docker_tag
-  source_type = var.source_type
-  buildspec = var.buildspec
-  source_location = var.source_location
-  cache_bucket = var.cache_bucket
+  build_docker_tag   = var.build_docker_tag
+  source_type        = var.source_type
+  buildspec          = var.buildspec
+  source_location    = var.source_location
+  cache_bucket       = var.cache_bucket
 }
 
 resource "aws_api_gateway_domain_name" "domain" {
-  domain_name = var.dns_name
+  domain_name     = var.dns_name
   certificate_arn = var.acm_arn
 }
 
 resource "aws_route53_record" "domain" {
   zone_id = var.route53_zone_id
-  name = aws_api_gateway_domain_name.domain.domain_name
-  type = "A"
+  name    = aws_api_gateway_domain_name.domain.domain_name
+  type    = "A"
 
   alias {
-    name = aws_api_gateway_domain_name.domain.cloudfront_domain_name
-    zone_id = aws_api_gateway_domain_name.domain.cloudfront_zone_id
+    name                   = aws_api_gateway_domain_name.domain.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.domain.cloudfront_zone_id
     evaluate_target_health = false
   }
 }
